d4d9f20e5b93cfdb238b71ca00a462e339f8c53a
clov1_s <chenghongkiu6@gmail.com>
2026-03-18 11:50:25 -0400

    php version 2

--- submit_feedback.php
+++ submit_feedback.php
@@ -1,22 +1,25 @@
 <?php
 
-$conn = new mysqli("localhost", "root", "team9062");
+$conn = new mysqli("localhost", "root", "", "team9062");
 
 if ($conn->connect_error) {
-    die("Connection failed". $conn->connect_error);
+    die("Connection failed: " . $conn->connect_error);
+}
+
+if (!isset($_POST['rating']) || !isset($_POST['feedback'])) {
+    die("Invalid form submission");
 }
 
 $rating = $_POST['rating'];
 $message = $_POST['feedback'];
 
-$sql =  "INSERT INTO feedback (rating, message)
-         VALUES ('$rating', '$message')";
+$sql = "INSERT INTO feedback (rating, message)
+        VALUES ('$rating', '$message')";
 
 if ($conn->query($sql) === TRUE) {
-    echo "Feedback sumitted successfully, thank you!";
-}
-else {
-    echo "Error " . $conn->error;
+    echo "Feedback submitted successfully, thank you!";
+} else {
+    echo "Error: " . $conn->error;
 }
 
 $conn->close();
