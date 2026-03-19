18a5c81fe8a164cda6581daa11c1fb13ad979ff5
Tr3x30 <frosthawkyt@gmail.com>
2026-03-18 23:39:38 -0400

    Homepage timer

---
+++ js/timer.js
@@ -0,0 +1,74 @@
+async function loadData() {
+    const response = await fetch("../database/competitions.json");
+    const data = await response.json();
+    return data;
+}
+
+function setTwoDigits(unitElement, value) {
+    const digits = unitElement.querySelectorAll(".digit");
+    const text = String(value).padStart(2, "0");
+
+    digits[0].textContent = text[0];
+    digits[1].textContent = text[1];
+}
+
+function updateCountdown(targetEpoch) {
+    const now = Date.now();
+    let diff = targetEpoch - now;
+
+    if (diff < 0) {
+        diff = 0;
+    }
+
+    const totalSeconds = Math.floor(diff / 1000);
+
+    const weeks = Math.floor(totalSeconds / (7 * 24 * 60 * 60));
+    const days = Math.floor((totalSeconds % (7 * 24 * 60 * 60)) / (24 * 60 * 60));
+    const hours = Math.floor((totalSeconds % (24 * 60 * 60)) / (60 * 60));
+    const mins = Math.floor((totalSeconds % (60 * 60)) / 60);
+    const secs = totalSeconds % 60;
+
+    const units = document.querySelectorAll("#timer .unit");
+
+    setTwoDigits(units[0], weeks);
+    setTwoDigits(units[1], days);
+    setTwoDigits(units[2], hours);
+    setTwoDigits(units[3], mins);
+    setTwoDigits(units[4], secs);
+}
+
+document.addEventListener("DOMContentLoaded", async () => {
+    let EPOCH = Date.now();
+    let EVENT = "9062 BUILD DAY";
+
+    const EVENTS = await loadData();
+
+    for (let key in EVENTS) {
+        const eventTime = Number(key);
+
+        if (eventTime > Date.now()) {
+            EPOCH = eventTime;
+            EVENT = EVENTS[key];
+            break;
+        }
+    }
+
+    const eventName = document.getElementById("event-name");
+    const computed = window.getComputedStyle(eventName)
+    eventName.textContent = EVENT;
+    console.log(computed);
+    console.log(computed.fontSize.replaceAll("px", "") * (computed.maxWidth.replaceAll("px", "") / computed.width.replaceAll("px", "")));
+
+    eventName.style.fontSize = computed.fontSize.replaceAll("px", "") * (computed.maxWidth.replaceAll("px", "") / computed.width.replaceAll("px", "")) + "px";
+
+
+    updateCountdown(EPOCH);
+
+    const timerInterval = setInterval(() => {
+        updateCountdown(EPOCH);
+
+        if (Date.now() >= EPOCH) {
+            clearInterval(timerInterval);
+        }
+    }, 1000);
+});
